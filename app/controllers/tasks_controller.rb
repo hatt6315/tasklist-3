90f4fa9c4ec3695ddac95ddbd15e29dc64ee282f
Ubuntu <ubuntu@ip-172-31-16-140.ap-northeast-1.compute.internal>
2021-10-04 06:31:35 +0000

    for heroku

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = current_user.tasks.build  # form_with 用
+      @tasks = Task.all
+  end
+
+  def show
+      
+  end
+
+  def new
+       @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+       
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+       
+  end
+
+  def update
+       
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+       
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+   private
+  def set_task
+    @task = Task find_by(params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+    
+  end 
+end
